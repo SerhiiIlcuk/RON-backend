911e0154710b3dc26b6a2cce0f3199b910455aaa
SerhiiIlchuk <surgijilchuk@gmail.com>
2020-06-18 14:21:07 +0300

    company profile update api

--- src/company/company.controller.ts
+++ src/company/company.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common';
-import { ApiBearerAuth, ApiCreatedResponse, ApiImplicitHeader, ApiOperation, ApiUseTags } from '@nestjs/swagger';
+import { Body, Controller, HttpCode, HttpStatus, Get, Post, Put, UseGuards, Param } from '@nestjs/common';
+import { ApiBearerAuth, ApiCreatedResponse, ApiImplicitHeader, ApiImplicitParam, ApiOkResponse, ApiOperation, ApiUseTags } from '@nestjs/swagger';
 import { CompanyService } from './company.service';
 import { CreateCompanyDto } from './dto/create-company.dto';
+import {UpdateCompanyDto} from './dto/update-company.dto';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthUser } from '../auth/decorators/user.decorator';
 
@@ -26,5 +27,35 @@ export class CompanyController {
   @ApiCreatedResponse({})
   async register(@Body() createCompanyDto: CreateCompanyDto, @AuthUser() user: any) {
     return await this.companyService.create(createCompanyDto, user.id);
+  }
+
+  @Put(':id')
+  @HttpCode(HttpStatus.OK)
+  @ApiOperation({ title: 'Update company' })
+  @ApiImplicitParam({name: 'id', description: 'id of company'})
+  @UseGuards(AuthGuard('jwt'))
+  @ApiBearerAuth()
+  @ApiImplicitHeader({
+    name: 'x-token',
+    description: 'the token we need for auth.',
+  })
+  @ApiOkResponse({})
+  async update(@Body() updateCompanyDto: UpdateCompanyDto, @Param() params) {
+    return await this.companyService.updateCompany(updateCompanyDto, params.id);
+  }
+
+  @Get(':id')
+  @HttpCode(HttpStatus.OK)
+  @ApiOperation({ title: 'Get company details' })
+  @ApiImplicitParam({name: 'id', description: 'id of company'})
+  @UseGuards(AuthGuard('jwt'))
+  @ApiBearerAuth()
+  @ApiImplicitHeader({
+    name: 'x-token',
+    description: 'the token we need for auth.',
+  })
+  @ApiOkResponse({})
+  async getCompanyDetails(@Param() params) {
+    return await this.companyService.get(params.id);
   }
 }
--- src/company/company.service.ts
+++ src/company/company.service.ts
@@ -1,9 +1,11 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model, Types } from 'mongoose';
 import {Company} from './interfaces/company.interface';
 import { CreateCompanyDto } from './dto/create-company.dto';
 import { User } from '../user/interfaces/user.interface';
+import { UpdateCompanyDto } from './dto/update-company.dto';
+import {log} from 'console';
 
 @Injectable()
 export class CompanyService {
@@ -24,6 +26,39 @@ export class CompanyService {
     };
     company.employees.push(employee);
     await company.save();
+    return company;
+  }
+
+  /**
+   * @description create company
+   */
+  async updateCompany(updateCompanyDto: UpdateCompanyDto, companyId: string): Promise<Company> {
+    const company = await this.companyModel.findById(companyId);
+    if (!company) {
+      throw new NotFoundException('Company not found');
+    }
+    const updatedCompany = Object.assign(company, updateCompanyDto);
+
+    try {
+      await this.companyModel.updateOne({_id: companyId}, updatedCompany);
+      return updatedCompany;
+    } catch (e) {
+      log(e);
+      throw new InternalServerErrorException('Server Error');
+    }
+  }
+
+  /**
+   * @param companyId
+   * @description get company by companyId
+   * @return company
+   */
+  async get(companyId: string): Promise<Company> {
+    const company = await this.companyModel.findById(companyId);
+
+    if (!company) {
+      throw new NotFoundException('Company Not Found');
+    }
     return company;
   }
 
